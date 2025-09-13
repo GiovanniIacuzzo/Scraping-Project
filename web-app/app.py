1784c23834704f6046b1adedb0cff1354bbfee8b
GiovanniIacuzzo <iacuzzogiovanni02@gmail.com>
2025-09-13 16:59:40 +0200

    modifhce

--- web-app/app.py
+++ web-app/app.py
@@ -6,11 +6,11 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'scra
 from dotenv import load_dotenv
 from flask_mail import Mail, Message
 from .db import collection
-from utils_github import follow_user_api, is_followed, extract_email_from_github_profile
-from scraping1.config import N_USERS, REQUEST_DELAY
-from scraping1.github_api import get_candidate_users_advanced, get_user_info
-from scraping1.scoring import score_user
-from scraping1.storage import save_user
+from .utils_github import follow_user_api, is_followed, extract_email_from_github_profile
+from .scraping1.config import N_USERS, REQUEST_DELAY
+from .scraping1.github_api import get_candidate_users_advanced, get_user_info
+from .scraping1.scoring import score_user
+from .scraping1.storage import save_user
 
 load_dotenv()
 
